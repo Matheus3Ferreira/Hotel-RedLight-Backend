dc5ff2070fbfe2ee3dc552627487ce199ceb4f1f
Matheus2798 <matheus.s.f@outlook.com.br>
2021-11-23 20:45:53 -0300

    fix: ComodidadeController status pattern

--- src/controller/ComodidadeController.ts
+++ src/controller/ComodidadeController.ts
@@ -1,28 +1,30 @@
 import { getRepository } from 'typeorm';
 import { Comodidade } from '../entity/Comodidade';
 import { Request, Response } from 'express';
+import { HttpResponse } from './response';
 
 export const getComodidades = async (request: Request, response: Response) => {
     const comodidade = await getRepository(Comodidade).find();
-    return response.json(comodidade);
+    return response.status(200).json(new HttpResponse<Comodidade[]>(comodidade, 200, 'Lista de Comodidades.'));
 }
 
 export const getComodidade = async (request: Request, response: Response) => {
     const { id } = request.params
     const comodidade = await getRepository(Comodidade).findOne(id)
-    return comodidade == undefined ? response.status(404).json('Usuario não localizado.') : response.status(200).json(comodidade);
+    return comodidade == undefined ? response.status(404).json(new HttpResponse<Comodidade>(null, 404, 'Comodidade não localizada')) : response.status(200).json(new HttpResponse<Comodidade>(comodidade, 200, 'Comodidade localizada'));
 };
 
 export const saveComodidade = async (request: Request, response: Response) => {
     const comodidade = await getRepository(Comodidade).save(request.body);
-    return response.status(201).json(comodidade);
+    return response.status(201).json(new HttpResponse<Comodidade>(comodidade, 201, 'Comodidade salva com sucesso'));
 }
 
 export const updateComodidade = async (request: Request, response: Response) => {
     const { id } = request.params
     const comodidade = await getRepository(Comodidade).update(id, request.body)
+    const findComodidade = await getRepository(Comodidade).findOne(id);
 
-    return comodidade.affected == 1 ? response.status(200).json('Comodidade alterada com sucesso.') : response.status(404).json('Comodidade não localizada.')
+    return comodidade.affected == 1 ? response.status(200).json(new HttpResponse<Comodidade>(findComodidade, 200, 'Comodidade alterada com sucesso')) : response.status(404).json(new HttpResponse<Comodidade>(null, 404, 'Comodidade não localizada'));
 
 };
 
@@ -30,7 +32,7 @@ export const deleteComodidade = async (request: Request, response: Response) => 
     const { id } = request.params
     const comodidade = await getRepository(Comodidade).delete(id)
 
-    return comodidade.affected == 1 ? response.status(200).json('Comodidade excluída com sucesso.') : response.status(404).json('Comodidade não localizada.')
+    return comodidade.affected == 1 ? response.status(204).json(new HttpResponse<Comodidade>(null, 204, 'Comodidade excluida com sucesso.')) : response.status(404).json(new HttpResponse<Comodidade>(null, 404, 'Comodidade não localizada'))
 
 };
 
