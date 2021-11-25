1db0bfab994c55ad4dd5f839b99c83ed2988fe02
Matheus2798 <matheus.s.f@outlook.com.br>
2021-11-24 23:50:54 -0300

    fix: http response pattern

--- src/controller/QuartoController.ts
+++ src/controller/QuartoController.ts
@@ -1,22 +1,23 @@
 import { getRepository } from 'typeorm';
 import { Quarto } from '../entity/Quarto';
 import { Request, Response } from 'express';
+import { HttpResponse } from './response';
 
 export const getQuartos = async (request: Request, response: Response) => {
     const quarto = await getRepository(Quarto).find({relations: ['comodidades']});
-    return response.json(quarto);
+    return response.json(new HttpResponse<Quarto[]>(quarto, 200, 'Quarto localizado.'));
 }
 
 export const getQuarto = async (request: Request, response: Response) => {
     const { id } = request.params
     const quarto = await getRepository(Quarto).findOne(id, {relations: ['comodidades']});
-    return quarto == undefined ? response.status(404).json('Quarto não localizado.') : response.status(200).json(quarto);
+    return quarto == undefined ? response.status(404).json(new HttpResponse<Quarto>(null, 404, 'Quarto não localizado.')) : response.status(200).json(new HttpResponse<Quarto>(quarto, 200, 'Quarto localizado.'));
 };
 
 export const saveQuarto = async (request: Request, response: Response) => {
 
     const quarto = await getRepository(Quarto).save(request.body);
-    return response.status(201).json(quarto);
+    return response.status(201).json(new HttpResponse<Quarto>(quarto, 201, 'Quarto criado com sucesso.'));
 }
 
 export const updateQuarto = async (request: Request, response: Response) => {
