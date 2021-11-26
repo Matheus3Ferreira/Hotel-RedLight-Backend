f7652968792fc05bbc25afe4a1d63c0a0dfdfc8c
Matheus2798 <matheus.s.f@outlook.com.br>
2021-11-26 01:02:09 -0300

    fix: saveReserva

--- src/controller/ReservaController.ts
+++ src/controller/ReservaController.ts
@@ -5,55 +5,22 @@ import { HttpResponse } from './response';
 import { Quarto } from '../entity/Quarto';
 
 export const getReservas = async (request: Request, response: Response) => {
-    const reserva = await getRepository(Reserva).find({relations: ['quartos'], order: {idReserva: "DESC"}});
+    const reserva = await getRepository(Reserva).find({ relations: ['quartos'], order: { idReserva: "DESC" } });
     return response.status(200).json(new HttpResponse<Reserva[]>(reserva, 200, 'Reservas listadas'));
 }
 
 export const getReserva = async (request: Request, response: Response) => {
     const { id } = request.params
-    const reserva = await getRepository(Reserva).findOne(id, {relations: ['quartos'], order: {idReserva: "DESC"}})
+    const reserva = await getRepository(Reserva).findOne(id, { relations: ['quartos'], order: { idReserva: "DESC" } })
     return reserva == undefined ? response.status(404).json(new HttpResponse<Reserva>(null, 404, 'Reserva não localizada listadas')) : response.status(200).json(new HttpResponse<Reserva>(reserva, 200, 'Reserva localizada'));
 };
 
-export const testReserva = async (request: Request, response: Response) => {
+export const saveReserva = async (request: Request, response: Response) => {
+
     const reserva = await getRepository(Reserva).save(request.body);
 
     return response.status(201).json(new HttpResponse<Reserva>(reserva, 201, 'Reserva salva com sucesso.'));
-}
 
-export const saveReserva = async (request: Request, response: Response) => {
-    
-    const { quartos, checkIn, checkOut } = request.body;
-
-    if (!quartos)
-        return response.status(400).json(new HttpResponse<Reserva>(null, 400, 'Quartos não informados.'));
-
-    let err = false;
-
-     quartos.forEach( async (quarto) => {
-        //Procurar nas reservas se dentro da data de checkin E checkout existe o quarto listado.
-        //1° pegar todas as reservas do periodo
-        //2° verificar se os quarto está em uma dessas reservas.
-        const verifyReservas = await getRepository(Reserva).find({
-            where: {
-                checkIn: MoreThanOrEqual(checkIn),
-                checkOut: LessThanOrEqual(checkOut)
-            },
-        });
-
-        console.log(verifyReservas)
-        return response.json("Ok")
-    })
-
-
-    //     if (verifyReservas)
-    //         console.log(verifyReservas)
-    //         return response.status(400).json(new HttpResponse<Reserva>(null, 400, 'Quarto já reservado.'));
-    // })
-
-    // const reserva = await getRepository(Reserva).save(request.body);
-
-    // return response.status(201).json(new HttpResponse<Reserva>(reserva, 201, 'Reserva salva com sucesso.'));
 }
 
 export const updateReserva = async (request: Request, response: Response) => {
